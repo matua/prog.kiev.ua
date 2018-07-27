41978e0e4383843baef8f78de30aa2315fe08a18
matua <matua@users.noreply.github.com>
2018-07-27 21:36:42 +0300

    OOP Lesson6 fixing task 3 via ExecutorService

--- src/OOP/Lesson6/Three/ShellSortingAlgorithm.java
+++ src/OOP/Lesson6/Three/ShellSortingAlgorithm.java
@@ -5,6 +5,8 @@ Created by matua on 22.07.2018 at 13:40
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ShellSortingAlgorithm {
     public static void shellSort(int[] array) throws InterruptedException {
@@ -20,19 +22,21 @@ public class ShellSortingAlgorithm {
         Thread thread = null;
 
         for (; h > 0 ;) {
-            thread = new Thread(new SortingThread(h, array), "Thread - " + String.valueOf(h));
-            thread.start();
+            ExecutorService service = Executors.newSingleThreadExecutor();
+            for (int i = 0; i < 100; i++) {
+                service.execute(new SortingThread(h, array));
+            }
             h = (h - 1) / 3;
-            thread.join();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        int[] unsortedArray = new int[25];
+        int[] unsortedArray = new int[2500];
         Random random = new Random();
         for (int i = 0; i < unsortedArray.length; i++) {
-            unsortedArray[i] = random.nextInt(10000);
+            unsortedArray[i] = random.nextInt(1000000);
         }
+
         System.out.println(Arrays.toString(unsortedArray));
         shellSort(unsortedArray);
         System.out.println(Arrays.toString(unsortedArray));
